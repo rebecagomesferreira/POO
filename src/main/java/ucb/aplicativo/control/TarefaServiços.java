d12e21c5f75eecd291c7c0152092ca0575755b68
rebecagomesferreira <rebecagomes2208@gmail.com>
2025-09-05 09:10:25 -0300

    Update TarefaServiços.java

--- src/main/java/ucb/aplicativo/control/TarefaServiços.java
+++ src/main/java/ucb/aplicativo/control/TarefaServiços.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ucb.aplicativo.control;
 
-/**
- *
- * @author rebeca.gomes
- */
 public class TarefaServiços {
     
 }
