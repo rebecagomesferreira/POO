ef84b9909b3467a7df10a5353f6a64ae1ba77752
rebecagomesferreira <rebecagomes2208@gmail.com>
2025-09-05 09:02:13 -0300

    Adição das classe nos pacotes

--- src/main/java/ucb/aplicativo/control/TarefaServiços.java
+++ src/main/java/ucb/aplicativo/control/TarefaServiços.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package ucb.aplicativo.model;
+package ucb.aplicativo.control;
 
 /**
  *
  * @author rebeca.gomes
  */
-public class Tarefas {
+public class TarefaServiços {
     
 }
