73b9e73aaef87fe516a5dc8f852fdb64df46a36e
rebecagomesferreira <rebecagomes2208@gmail.com>
2025-09-05 09:10:08 -0300

    Update AppToDoList.java

--- src/main/java/ucb/aplicativo/cli/AppToDoList.java
+++ src/main/java/ucb/aplicativo/cli/AppToDoList.java
@@ -1,16 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package ucb.aplicativo.cli;
 
-/**
- *
- * @author rebeca.gomes
- */
 public class AppToDoList {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
     }
 }
